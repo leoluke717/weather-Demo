e9bb943859dace911d0b29fa614e9912635074dd
marycctv <546678108@qq.com>
2016-01-01 14:30:24 +0800

    Initial Commit

---
+++ 天气预报/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  天气预报
+//
+//  Created by mac on 16/1/1.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 天气预报/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  天气预报
+//
+//  Created by mac on 16/1/1.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
